7646f5194b5686bdfba2c2da4af618fbc7e66023
José Wilson de Lima <josewilson791@gmail.com>
2022-10-20 11:47:49 -0300

    Finalizado aula 01

--- src/main.js
+++ src/main.js
@@ -1,1 +1,24 @@
 import "./css/index.css"
+
+// DOM para as cores do cartão
+const ccBgColor01 = document.querySelector(".cc-bg svg > g g:nth-child(1) path")
+const ccBgColor02 = document.querySelector(".cc-bg svg > g g:nth-child(2) path")
+
+// DOM para icone do cartão
+const ccLogo = document.querySelector(".cc-logo span:nth-child(2) img")
+
+// Função para mudar as cores e o icone do cartão
+function setCardType(type) {
+  // Estrutura de dados para as cores
+  const colors = {
+    visa: ["#436D99", "#2D57F2"],
+    mastercard: ["#DF6F29", "#C69347"],
+    default: ["black", "gray"],
+  }
+
+  ccBgColor01.setAttribute("fill", colors[type][0])
+  ccBgColor02.setAttribute("fill", colors[type][1])
+  ccLogo.setAttribute("src", `cc-${type}.svg`)
+}
+
+globalThis.setCardType = setCardType
